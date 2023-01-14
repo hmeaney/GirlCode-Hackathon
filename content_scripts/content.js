dccdf4ebb5310ea5b37d9dc0c82dd8a01d2dc5a9
Nara Iamsakun <nara.iamsakun@gmail.com>
2023-01-14 00:25:11 -0800

    get price and name

---
+++ content_scripts/content.js
@@ -0,0 +1,6 @@
+var productName = document.getElementsByClassName("js-product-detail__product-name f2")[0].innerText;
+var productPrice = document.getElementsByClassName("price-default")[0].innerText;
+
+console.log(productName)
+console.log(productPrice)
+
