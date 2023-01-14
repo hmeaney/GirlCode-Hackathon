d15ddf959f9e1983f72747d4b80fec48fb4415d8
kelly-zhu <79768096+kelly-zhu@users.noreply.github.com>
2023-01-14 13:16:48 -0800

    edit pop-up

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,8 +1,57 @@
-var productName = document.getElementsByClassName("js-product-detail__product-name f2")[0].innerText;
-var productPrice = document.getElementsByClassName("price-default")[0].innerText;
+'use strict';
+window.speechSynthesis.speak(
+    new SpeechSynthesisUtterance("content page loaded")
+ );
+ 
+ function getFullName(){
+    window.speechSynthesis.speak(
+		new SpeechSynthesisUtterance("content page loaded")
+	 );
+    
+    var productName = document.getElementsByClassName("js-product-detail__product-name f2")[0].innerText;
 
-console.log(productName)
-console.log(productPrice)
+    if(document.getElementsByClassName("price-default")[0] != null){
+        var productPrice = document.getElementsByClassName("price-default")[0].innerText;
 
-chrome.tts.speak('Hello, world');
+    }
+    console.log(productName)
+    console.log(productPrice)
+    return productName;
+}
 
+export default {getFullName};
+
+
+
+// function createProduct() {
+//     // var name = document.getElementsByClassName("js-product-detail__product-name f2")[0].innerText;
+//     // var productPrice = document.getElementsByClassName("price-default")[0].innerText;
+//     // var color = "blue";
+
+//     // var descr = name + " costs " + productPrice + " with colour " + color;
+//     // document.body.innerHTML = "<h1>The product is:  " + descr + "</h1>"
+//     // document.getElementByID('product-descr').innerHTML = "test";
+//     // console.log("done product descr");
+
+//     if(document.getElementsByClassName("s-product-detail__product-name f2")[0] != null){
+//         var productName = document.getElementsByClassName("js-product-detail__product-name f2")[0].innerText;
+//     }
+    
+//     if(document.getElementsByClassName("price-default")[0] != null){
+//         var productPrice = document.getElementsByClassName("price-default")[0].innerText;
+
+//     }
+
+//     window.speechSynthesis.speak(
+// 		new SpeechSynthesisUtterance("content page testing")
+// 	 );
+    
+//     console.log(productName)
+//     console.log(productPrice)
+//     var descr = "product name is: " + productName;
+//     document.getElementById("product-descr").innerHTML = descr;
+// }
+
+// chrome.runtime.sendMessage(greeting: "hello", function(response){
+//     console.log(response.farewell);
+// })
