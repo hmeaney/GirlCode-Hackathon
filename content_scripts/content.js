26426c7b5fde1a55dc94a9f1fc937153b1bafcc0
Nara Iamsakun <nara.iamsakun@gmail.com>
2023-01-14 14:16:04 -0800

    added texts

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,10 +1,10 @@
 // name price and fit description of the product
-var productName = document.getElementsByClassName("js-product-detail__product-name f2")[0].innerText;
-var productPrice = document.getElementsByClassName("price-default")[0].innerText;
-var productSummary = document.getElementsByClassName("js-product-accordion__content")[0].textContent;
+var productName = "This product is called " + document.getElementsByClassName("js-product-detail__product-name f2")[0].innerText;
+var productPrice = "This product costs " + document.getElementsByClassName("price-default")[0].innerText;
+var productSummary = "Some features of the product are " + document.getElementsByClassName("js-product-accordion__content")[0].textContent;
 
 try {
-    var productFit = document.getElementsByClassName("fit")[0].innerHTML;
+    var productFit = "The product fit is " + document.getElementsByClassName("fit")[0].innerHTML;
 }
 catch (err) {
     var productFit = "There is no fit description provided";
