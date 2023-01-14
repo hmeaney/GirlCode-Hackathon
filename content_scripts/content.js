f65b0e01a86ee8e9bd3364efa68d69e7e88e5054
Nara Iamsakun <nara.iamsakun@gmail.com>
2023-01-14 14:02:40 -0800

    switching branch

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,6 +1,19 @@
+// name price and fit description of the product
 var productName = document.getElementsByClassName("js-product-detail__product-name f2")[0].innerText;
 var productPrice = document.getElementsByClassName("price-default")[0].innerText;
+var productSummary = document.getElementsByClassName("js-product-accordion__content")[0].textContent;
 
-console.log(productName)
-console.log(productPrice)
+try {
+    var productFit = document.getElementsByClassName("fit")[0].innerHTML;
+}
+catch (err) {
+    var productFit = "There is no fit description provided";
+}
+
+
+// debugging
+// console.log(productName)
+// console.log(productPrice)
+// console.log(productSummary);
+// console.log(productFit);
 
